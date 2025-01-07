7ff692fb3184a56c5afb73f3f8f5a498b7d02594
sorochinskii <pika8oo@yandex.ru>
2025-01-07 21:35:46 +0500

    First

---
+++ source/api/v1/app.py
@@ -0,0 +1,21 @@
+from config import settings
+from fastapi import FastAPI
+from utils import URLBuilder
+
+url_builder = URLBuilder(
+    host=settings.HOST,
+    protocol=settings.HTTP_SECURE,
+    port=settings.HTTP_PORT)
+url = url_builder.url()
+tags_metadata = [
+    {
+        'name': 'v1',
+        "description": "API version 1",
+        'externalDocs': {
+            'description': 'sub-docs',
+            'url': url
+        }
+    },
+]
+
+app = FastAPI(openapi_tags=tags_metadata)
---
+++ source/config.py
@@ -0,0 +1,32 @@
+from dotenv import find_dotenv
+from pydantic import Field
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    def __init__(self, env_file):
+        super().__init__(_env_file=env_file, _case_sensitive=True, )
+
+    HOST: str = Field(default='HOST')
+    SSH_PRIVATE_KEY: str = Field(default='SSH_KEY')
+    SSH_PASSPHRASE: str = Field(default='SSH_PASSPHRASE')
+    SSH_PORT: str = Field(default='SSH_PORT')
+    PROJECT_NAME: str = Field(default='PROJECT_NAME')
+    LOG_DIR: str = Field(default='LOG_DIR')
+    V1: str = Field(default='V1')
+    HTTP_SECURE: str = Field(default='HTTP_SECURE')
+    HTTP_PORT: int = Field(default=80)
+
+    DB_HOST: str = Field(default='DB_HOST')
+    DB_PASS: str = Field(default='DB_PASS')
+    DB_PORT: str = Field(default='DB_PORT')
+    DB_USER: str = Field(default='DB_USER')
+    DB_NAME: str = Field(default='DB_NAME')
+    # DB_URL: str = Field(default='DB_URL')
+
+    class Config:
+        validate_assignment = True
+
+
+envs = find_dotenv('.env', raise_error_if_not_found=True)
+settings = Settings(env_file=envs)
